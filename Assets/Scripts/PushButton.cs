207877fdb639dcb2d10ce3a151f119a4059581c0
“TheZhoon” <marin.pavic@five.agency>
2020-03-12 21:20:23 +0100

    add push button and add glass ball

---
+++ Assets/Scripts/PushButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+    Animator animator;
+    void Awake() {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player")){
+            animator.SetBool("isPressed", true);
+        }
+       
+    }
+    void OnCollisionExit(Collision collision)
+    {  
+        if(collision.gameObject.CompareTag("Player")){
+            animator.SetBool("isPressed", false);
+        }
+    }
+}
