52412797de24befdd116d013297ba26c291e96bb
“TheZhoon” <marin.pavic@five.agency>
2020-03-12 18:52:40 +0100

    add alternate controller scheme

---
+++ Assets/Scripts/RotateGyro.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGyro: MonoBehaviour
+{
+
+    public int speed;
+    private Quaternion _origin = Quaternion.identity;
+
+
+    private void getOrigin()
+    {
+        _origin = Input.gyro.attitude;
+    }
+
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+        getOrigin();
+    }
+
+    void Update()
+    {
+        float transX = Input.GetAxis("Horizontal") * speed;
+        float transY = Input.GetAxis("Vertical") * speed;
+        
+        transform.Rotate(transY * Time.deltaTime,0, transX * Time.deltaTime);
+        //this.transform.rotation = ConvertRightHandedToLeftHandedQuaternion(Input.gyro.attitude);
+    }
+
+    private Quaternion ConvertRightHandedToLeftHandedQuaternion(Quaternion rightHandedQuaternion)
+    {
+        return new Quaternion(-rightHandedQuaternion.x,
+                               -rightHandedQuaternion.z,
+                               -rightHandedQuaternion.y,
+                                rightHandedQuaternion.w);
+    }
+}
